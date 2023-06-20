2147311c75f46da927b15e3bb1ccaebc4074fa76
Eldino316 <95032632+eldino316@users.noreply.github.com>
2023-06-20 18:21:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -85,18 +85,7 @@ def train_classifier(nbr):
     for image in path:
         img = Image.open(image).convert('L');
         imageNp = np.array(img, 'uint8')
-        id = int(os.path.split(image)[1].split(".")[1])
- 
-        faces.append(imageNp)
-        ids.append(id)
-    ids = np.array(ids)
- 
-    # Train the classifier and save
-    clf = cv2.face.LBPHFaceRecognizer_create()
-    clf.train(faces, ids)
-    clf.write("classifier.xml")
- 
-    return redirect('/')
+        id = int(os.path.split(imag
  
  
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Face Recognition >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
